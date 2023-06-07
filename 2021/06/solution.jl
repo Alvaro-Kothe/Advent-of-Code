3be3817bebc9087a899a3cf0cc0027e3c28d1322
Alvaro-Kothe <kothe65@gmail.com>
2023-06-07 16:27:42 -0300

    Add day 06 code

---
+++ 2021/06/solution.jl
@@ -0,0 +1,28 @@
+function parse_data(filepath::String = joinpath(@__DIR__, "example.txt"))
+    fish_timers = parse.(Int, split(readline(filepath), ","))
+    return fish_timers
+end
+
+function simulate_lanternfish(start_timers, days)
+    cycle_counter = [count(==(n), start_timers) for n = 0:8]
+    for _ = 1:days
+        n_new_fish = popfirst!(cycle_counter)
+        cycle_counter[7] += n_new_fish
+        push!(cycle_counter, n_new_fish)
+
+    end
+    return cycle_counter
+end
+
+function part1()
+    data = parse_data(joinpath(@__DIR__, "input.txt"))
+    return sum(simulate_lanternfish(data, 80))
+end
+
+function part2()
+    data = parse_data(joinpath(@__DIR__, "input.txt"))
+    return sum(simulate_lanternfish(data, 256))
+end
+
+println(part1())
+println(part2())
