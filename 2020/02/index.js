3357d6dffd1701689d2396fa2dda8e463aae96cc
Alvaro-Kothe <kothe65@gmail.com>
2023-08-20 20:58:22 -0300

    Add day 02 year 2020 code

---
+++ 2020/02/index.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+
+const data = fs
+  .readFileSync("input.txt")
+  .toString()
+  .split("\n")
+  .filter(Boolean)
+
+// Part 1
+let valid = 0
+let valid2 = 0;
+
+re = /(\d+)-(\d+) (\w): (.+)/
+for (let i = 0; i < data.length; i++) {
+  const line = data[i];
+  let [_, min, max, ch, pass] = line.match(re);
+  min = Number(min)
+  max = Number(max)
+  const ch_occurrences = (pass.match(RegExp(ch, "g")) || []).length;
+  if (min <= ch_occurrences && ch_occurrences <= max) {
+    valid += 1;
+  }
+
+  if (pass[min - 1] == ch ^ pass[max - 1] == ch) {
+    valid2 += 1;
+  }
+}
+
+console.log(valid);
+console.log(valid2);
