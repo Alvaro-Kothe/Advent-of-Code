77057c63514e272b9f04167bc0f583ffc906d1f0
Alvaro-Kothe <kothe65@gmail.com>
2023-12-07 17:41:01 -0300

    Add day 1 part 2 year 2023

--- 2023/bin/day01.ml
+++ 2023/bin/day01.ml
@@ -25,3 +25,33 @@ let () =
   let numbers = List.map data ~f:get_first_last_digit in
   printf "Part 1: %d\n" (sum numbers)
 ;;
+
+let rec _replace_cases cases str =
+  match cases with
+  | [] -> str
+  | (case, repl) :: rest ->
+    String.substr_replace_all ~pattern:case ~with_:repl (_replace_cases rest str)
+;;
+
+let replace_cases str =
+  let cases_replacements =
+    [ "one", "one1one"
+    ; "two", "two2two"
+    ; "three", "three3three"
+    ; "four", "four4four"
+    ; "five", "five5five"
+    ; "six", "six6six"
+    ; "seven", "seven7seven"
+    ; "eight", "eight8eight"
+    ; "nine", "nine9nine"
+    ]
+  in
+  _replace_cases cases_replacements str
+;;
+
+let () =
+  let numbers =
+    List.map data ~f:(fun line -> replace_cases line |> get_first_last_digit)
+  in
+  printf "Part 2: %d\n" (sum numbers)
+;;
