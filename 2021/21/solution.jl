751259fc0e5d26ebd3341adf35a085cfd6952b3a
Alvaro-Kothe <kothe65@gmail.com>
2023-12-30 12:05:39 -0300

    fix day21

--- 2021/21/solution.jl
+++ 2021/21/solution.jl
@@ -1,4 +1,4 @@
-function parse_data(filepath::String = joinpath(@__DIR__, "example.txt"))
+function parse_data(filepath = joinpath(@__DIR__, "example.txt"))
     pattern = r"Player (\d+) starting position: (\d+)"
     d = Dict{Int,Vector{Int}}()
     for line in eachline(filepath)
@@ -29,10 +29,11 @@ end
 cache = Dict{NTuple{4,Int},Vector{Int}}()
 function count_wins(
     pos1::T,
-    pos2::T;
-    score1::T = 0,
-    score2::T = 0,
+    pos2::T,
+    score1::T,
+    score2::T,
 )::AbstractVector{T} where {T<:Integer}
+    score1 >= 21 && return [1, 0]
     score2 >= 21 && return [0, 1]
 
     cur_state = (pos1, pos2, score1, score2)
@@ -42,18 +43,22 @@ function count_wins(
     wins = zeros(T, 2)
     for r1 in 1:3, r2 in 1:3, r3 in 1:3
         new_pos = mod1(pos1 + r1 + r2 + r3, 10)
-        new_score = score1 + new_pos
-        wins += count_wins(pos2, new_pos, score1 = score2, score2 = new_score) |> reverse
+        p2w, p1w = count_wins(pos2, new_pos, score2, score1 + new_pos)
+        wins += [p1w, p2w]
     end
     cache[cur_state] = wins
     return wins
 end
 
 function part2(data)::Integer
-    wins = count_wins(data[1][1], data[2][1])
+    wins = count_wins(data[1][1], data[2][1], 0, 0)
     return maximum(wins)
 end
 
+test_data = parse_data(IOBuffer("""Player 1 starting position: 4
+Player 2 starting position: 8"""))
+@assert part2(test_data) == 444356092776315
+
 data = parse_data(joinpath(@__DIR__, "input.txt"))
-println(part1(data))
+println(part1(deepcopy(data)))
 @time println(part2(data))
