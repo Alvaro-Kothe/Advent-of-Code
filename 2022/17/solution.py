527a6eea7409e9e1a77366c6ebf518294fae937d
Alvaro-Kothe <kothe65@gmail.com>
2023-12-31 17:30:00 -0300

    Fix newline input

--- 2022/17/solution.py
+++ 2022/17/solution.py
@@ -9,15 +9,17 @@ ROCKS = [
 
 def parse_data(filepath="input.txt"):
     with open(filepath) as file:
-        return [1 if ch == ">" else -1 for ch in file.read()]
+        return [1 if ch == ">" else -1 for ch in file.read().rstrip()]
 
 
 def simulate(n_rocks, hot_gas_data):
     ground_width = 7
     height = 0
 
-    rock_idx = jet_idx = 0
+    rock_idx = 0
+    jet_idx = 0
     blocked_positions = set()
+    cache = dict()
 
     def is_available(pos: complex):
         return (
@@ -26,42 +28,36 @@ def simulate(n_rocks, hot_gas_data):
             and pos not in blocked_positions
         )
 
-    def valid_move(pos: complex, dir: complex, rock: list[complex]):
+    def valid_move(pos: complex, dir: complex, rock):
         return all(is_available(pos + dir + x) for x in rock)
 
-    cache = {}
-
     for i in range(n_rocks):
-        pos = complex(2, height + 4)
-
-        key = rock_idx, jet_idx
-        # if key in cache:
-        #     cached_step, cached_height = cache[key]
-        #     remaining_rocks = n_rocks - i
-        #     steps_since_cache = i - cached_step
-        #
-        #     div, rem = divmod(remaining_rocks, steps_since_cache)
-        #     is_cycle = rem == 0
-        #     if is_cycle:
-        #         return int(height + (height - cached_height) * div)
-        # else:
-        #     cache[key] = i, height
-
+        pos = 2 + (height + 4) * 1j
         rock = ROCKS[rock_idx]
         rock_idx = (rock_idx + 1) % len(ROCKS)
+        key = rock_idx, jet_idx
+        if key in cache:
+            cached_step, cached_height = cache[key]
+            remaining_rocks = n_rocks - i
+            steps_since_cache = i - cached_step
+
+            div, rem = divmod(remaining_rocks, steps_since_cache)
+            is_cycle = rem == 0
+            if is_cycle:
+                return int(height + (height - cached_height) * div)
+        else:
+            cache[key] = i, height
         while True:
             jet = hot_gas_data[jet_idx]
             jet_idx = (jet_idx + 1) % len(hot_gas_data)
-
             if valid_move(pos, jet, rock):
                 pos += jet
             if valid_move(pos, -1j, rock):
                 pos += -1j
             else:
-                rock_height = max(rock, key=lambda x: x.imag).imag + pos.imag
-                height = max(height, rock_height)
-                blocked_positions.update([pos + x for x in rock])
                 break
+        blocked_positions.update(pos + x for x in rock)
+        height = max(x.imag for x in blocked_positions)
 
     return int(height)
 
@@ -72,12 +68,10 @@ def part1():
 
 
 def part2():
-    # Height increments constant between rock and jet cycles.
     data = parse_data("input.txt")
-    n_sim = 1000000000000
-    return simulate(n_sim, data)
+    return simulate(1000000000000, data)
 
 
 if __name__ == "__main__":
     print(part1())
-    # print(part2())
+    print(part2())
