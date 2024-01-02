70252d4fccd38bc9a4847267659bd8c6efb0c04f
Alvaro-Kothe <kothe65@gmail.com>
2024-01-02 11:44:19 -0300

    Add day 01

---
+++ 2019/src/day01.cpp
@@ -0,0 +1,27 @@
+#include "iostream"
+#include <fstream>
+using namespace std;
+int compute_fuel(const int x) { return (x / 3 - 2); }
+int compute_fuel2(const int x) {
+  int fuel = compute_fuel(x);
+  if (fuel <= 0) {
+    return 0;
+  }
+  return fuel + compute_fuel2(fuel);
+}
+
+int main() {
+  int p1 = 0;
+  int p2 = 0;
+  std::ifstream f_in("input/day01.txt");
+  string line;
+  int mass;
+  while (getline(f_in, line)) {
+    mass = stoi(line);
+    p1 += compute_fuel(mass);
+    p2 += compute_fuel2(mass);
+  }
+  cout << "Part1: " << p1 << endl;
+  cout << "Part2: " << p2 << endl;
+  return 0;
+}
