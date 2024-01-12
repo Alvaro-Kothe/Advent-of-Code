e8ccca5d36e343b14ec979f7c53bef9d276ecacf
Alvaro-Kothe <kothe65@gmail.com>
2024-01-12 14:22:53 -0300

    Add day 19

---
+++ 2019/src/day19.cpp
@@ -0,0 +1,52 @@
+#include "intcode.h"
+#include <cstddef>
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <ostream>
+#include <string>
+
+template <typename T, typename U>
+std::pair<T, U> operator+(const std::pair<T, U> &l, const std::pair<T, U> &r) {
+  return {l.first + r.first, l.second + r.second};
+}
+
+bool check_position(memory_t program, std::size_t x, std::size_t y) {
+  std::queue<int64_t> queue;
+  queue.push(x);
+  queue.push(y);
+  return Intcode::IntcodeProgram(program).run_program(queue);
+}
+
+using position_t = std::pair<int, int>;
+std::vector<position_t> get_affected_points(memory_t program,
+                                            std::size_t limit) {
+  std::vector<position_t> out;
+  for (size_t x = 0; x < limit; ++x) {
+    for (size_t y = 0; y < limit; ++y) {
+      if (check_position(program, x, y))
+        out.push_back({x, y});
+    }
+  }
+  return out;
+}
+
+int main(int argc, char **argv) {
+  std::string filepath = argc > 1 ? argv[1] : "input/day19.txt";
+  std::ifstream fh(filepath);
+  if (!fh.is_open()) {
+    std::cerr << "File not found\n";
+    return 1;
+  }
+  const memory_t program = Intcode::parse_data(fh);
+  const auto affected_points = get_affected_points(program, 50);
+  std::cout << "Part1: " << affected_points.size() << '\n';
+  uint32_t x = 0, y = 0;
+  while (!check_position(program, x + 99, y)) {
+    y++;
+    while (!check_position(program, x, y + 99))
+      x++;
+  }
+  std::cout << "Part2: " << x * 10000 + y << '\n';
+  return 0;
+}
