dee8190cdcf05574f61a3d9741d6e5696c09e1e4
Alvaro-Kothe <kothe65@gmail.com>
2024-01-21 10:38:39 -0300

    Add day 19 part2

--- 2020/src/day19.js
+++ 2020/src/day19.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const filename_ = process.argv[2] || "tst";
+const filename_ = process.argv[2] || "input/day19.txt";
 const data = fs.readFileSync(filename_).toString().trim().split("\n\n");
 
 const rule_raw = data[0];
@@ -21,32 +21,43 @@ function parse_rules(str) {
 
 const rules = parse_rules(rule_raw);
 
-function match_sequence(rules, seq, message) {
-  let rest = message;
-  for (const ruleid of seq) {
-    rest = match_rule(rules, ruleid, rest);
-    if (rest === null) return null;
-  }
-  return rest;
+function* match_sequence(rules, seq, message) {
+  if (seq.length === 0) yield message;
+  else
+    for (let rest of match_rule(rules, seq[0], message))
+      yield* match_sequence(rules, seq.slice(1), rest);
 }
 
-function match_option(rules, options, message) {
+function* match_option(rules, options, message) {
   for (const seq of options) {
-    const rest_str = match_sequence(rules, seq, message);
-    if (rest_str !== null) return rest_str;
+    yield* match_sequence(rules, seq, message);
   }
-  return null;
 }
 
-function match_rule(rules, ruleid, message) {
+function* match_rule(rules, ruleid, message) {
   const rule = rules.get(ruleid);
-  if (typeof rule === "string")
-    return message.startsWith(rule) ? message.slice(rule.length) : null;
-  return match_option(rules, rule, message);
+  if (typeof rule === "string" && message.startsWith(rule))
+    yield message.slice(rule.length);
+  else if (Array.isArray(rule)) yield* match_option(rules, rule, message);
 }
 
-console.log(
-  "Part1: ",
-  messages.reduce((acc, msg) => acc + (match_rule(rules, "0", msg) === ""), 0),
-);
-console.log("Part2: ", -1);
+function match(rules, rule_id, msg) {
+  for (const m of match_rule(rules, rule_id, msg)) {
+    if (m === "") return true;
+  }
+  return false;
+}
+
+function count_match0(messages, rules) {
+  return messages.reduce((acc, msg) => acc + match(rules, "0", msg), 0);
+}
+
+const part2_rules = new Map(rules);
+part2_rules.set("8", [["42"], ["42", "8"]]);
+part2_rules.set("11", [
+  ["42", "31"],
+  ["42", "11", "31"],
+]);
+
+console.log("Part1: ", count_match0(messages, rules));
+console.log("Part2: ", count_match0(messages, part2_rules));
