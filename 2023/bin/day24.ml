0df5d65e4dc3a622254d0e69345045e7369f2578
Alvaro-Kothe <kothe65@gmail.com>
2024-03-31 01:47:34 -0300

    Proper solution to day 24

--- 2023/bin/day24.ml
+++ 2023/bin/day24.ml
@@ -51,31 +51,128 @@ let () =
   chech_inter range range pv |> Printf.printf "Part1: %d\n"
 ;;
 
+(*
+   Solve A x = b
+   where A = [
+[dvy, -dvx, 0, -dy, dx, 0]
+[dvz, 0, -dvx, -dz, 0, dx]
+]
+   b = [
+d(xvy) - d(yvx)
+d(xvz) - d(zvx)
+]
+   x = (x, y, z, vx, vy, vz) from the rock
+   x0, y0, ... represents the hailstone values from a preceding
+   x1, y1, ... represents the values of the next
+   find x solving:
+   A x = b
+*)
+let computeAb ((x1, y1, z1), (vx1, vy1, vz1)) ((x2, y2, z2), (vx2, vy2, vz2)) =
+  let dx = x2 -. x1 in
+  let dy = y2 -. y1 in
+  let dz = z2 -. z1 in
+  let dvx = vx2 -. vx1 in
+  let dvy = vy2 -. vy1 in
+  let dvz = vz2 -. vz1 in
+  let d_x_vy = (x2 *. vy2) -. (x1 *. vy1) in
+  let d_y_vx = (y2 *. vx2) -. (y1 *. vx1) in
+  let d_x_vz = (x2 *. vz2) -. (x1 *. vz1) in
+  let d_z_vx = (z2 *. vx2) -. (z1 *. vx1) in
+  let matrix_A =
+    [ [| dvy; -.dvx; 0.; -.dy; dx; 0. |]; [| dvz; 0.; -.dvx; -.dz; 0.; dx |] ]
+  in
+  let b = [ d_x_vy -. d_y_vx; d_x_vz -. d_z_vx ] in
+  matrix_A, b
+;;
+
+let generate_Ab nelements lst =
+  let rec getAb_list n accA accb = function
+    | _ when n = 0 -> accA, accb
+    | x :: (y :: _ as t) ->
+      let a, b = computeAb x y in
+      getAb_list (n - 1) (a @ accA) (b @ accb) t
+    | _ -> assert false
+  in
+  let acca, accb = getAb_list nelements [] [] lst in
+  Array.of_list acca, Array.of_list accb
+;;
+
+module Array = struct
+  include Array
+
+  (* Computes: f a.(0) + f a.(1) + ... where + is 'g'. *)
+  let foldmap g f a =
+    let n = Array.length a in
+    let rec aux acc i = if i >= n then acc else aux (g acc (f a.(i))) (succ i) in
+    aux (f a.(0)) 1
+  ;;
+end
+
+let foldmap_range g f (a, b) =
+  let rec aux acc n =
+    let n = succ n in
+    if n > b then acc else aux (g acc (f n)) n
+  in
+  aux (f a) a
+;;
+
+let fold_range f init (a, b) =
+  let rec aux acc n = if n > b then acc else aux (f acc n) (succ n) in
+  aux init a
+;;
+
+let swap_elem m i j =
+  let x = m.(i) in
+  m.(i) <- m.(j);
+  m.(j) <- x
+;;
+
+let maxtup a b = if snd a > snd b then a else b
+let augmented_matrix m b = Array.(init (length m) (fun i -> append m.(i) [| b.(i) |]))
+
+(* https://rosettacode.org/wiki/Gaussian_elimination#OCaml *)
+(* Solve Ax=b for x, using gaussian elimination with scaled partial pivot,
+ * and then back-substitution of the resulting row-echelon matrix. *)
+let solve m b =
+  let n = Array.length m in
+  let n' = pred n in
+  (* last index = n-1 *)
+  let s = Array.(map (foldmap max abs_float) m) in
+  (* scaling vector *)
+  let a = augmented_matrix m b in
+  for k = 0 to pred n' do
+    (* Scaled partial pivot, to preserve precision *)
+    let pair i = i, abs_float a.(i).(k) /. s.(i) in
+    let i_max, v = foldmap_range maxtup pair (k, n') in
+    if v < epsilon_float then failwith "Matrix is singular.";
+    swap_elem a k i_max;
+    swap_elem s k i_max;
+    (* Eliminate one column *)
+    for i = succ k to n' do
+      let tmp = a.(i).(k) /. a.(k).(k) in
+      for j = succ k to n do
+        a.(i).(j) <- a.(i).(j) -. (tmp *. a.(k).(j))
+      done
+    done
+  done;
+  (* Backward substitution; 'b' is in the 'nth' column of 'a' *)
+  let x = Array.copy b in
+  (* just a fresh array of the right size and type *)
+  for i = n' downto 0 do
+    let minus_dprod t j = t -. (x.(j) *. a.(i).(j)) in
+    x.(i) <- fold_range minus_dprod a.(i).(n) (i + 1, n') /. a.(i).(i)
+  done;
+  x
+;;
+
 let () =
   let hailstones = parse_data data in
-  let file_out = "day24.ae" in
-  let oc = open_out file_out in
-  Printf.fprintf oc "logic x, y, z, vx, vy, vz : real\n";
-  Seq.iteri
-    (fun idx ((x, y, z), (vx, vy, vz)) ->
-      Printf.fprintf
-        oc
-        "axiom a%d : (x - %f) * (%f - vy) = (y - %f) * (%f - vx)\n"
-        idx
-        x
-        vy
-        y
-        vx;
-      Printf.fprintf
-        oc
-        "axiom b%d : (y - %f) * (%f - vz) = (z - %f) * (%f - vy)\n"
-        idx
-        y
-        vz
-        z
-        vy;
-      ())
-    (List.to_seq hailstones |> Seq.take 10);
-  Printf.fprintf oc "check_sat c : x + y + z > 0.0\n";
-  close_out oc
+  let a, b = generate_Ab 3 hailstones in
+  let x = solve a b in
+  (* let _ = *)
+  (*   Array.iter (fun x -> Printf.printf "%f " x) x; *)
+  (*   print_newline *)
+  (* in *)
+  let p2 = x.(0) +. x.(1) +. x.(2) in
+  Printf.printf "Part2: %f\n" p2
 ;;
